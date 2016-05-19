b032e5cd874875a8a8c2b75a951f4f4759198444
lacunahag <jasmine.yhumbert@gmail.com>
2016-05-19 12:55:28 -0700

    first commit

---
+++ madlib.py
@@ -0,0 +1,24 @@
+"""
+Students are expected to comply with the PDX Code Guild Code of Conduct. Students are given a copy of the PDX Code Guild code of conduct and a copy of the school catalog containing the Code of Conduct and Policy on Code of Conduct Infringement upon registration. Please refer to your copy of the code of conduct and Policy on Code of Conduct Infringement sections of the school catalog for more information."""
+
+print ("Welcome to this mad lib: please enter a plural noun")
+# plural_noun1 = input("enter a plural noun:")
+# plural_noun2 = input("enter a plural noun:")
+# noun1 = input("enter a noun:")
+# noun2 = input("enter a noun:")
+# noun3 = input("enter a noun:")
+# verb1 = input("enter a verb:")
+# verb2 = input("enter a verb:")
+# verb3 = input("enter a verb:")
+
+plural_noun1 = "mercenaries"
+plural_noun2 = "hats"
+noun1 = "computer"
+noun2 = "apple"
+noun3 = "foot"
+verb1 = "fly"
+verb2 = "swim"
+verb3 = "eat"
+
+
+print (plural_noun1, " are expected to ", verb1, " with the PDX ", noun3, " of ",noun2, " ", noun1," of Conduct. ", plural_noun1, " are given a copy of the PDX ", noun3, " of ",noun2, ", ", noun1, " of conduct and a copy of the school catalog containing the ", noun1, " of Conduct and Policy on ", noun1, " of Conduct Infringement upon registration. Please ", verb2, " to your copy of the ", noun1, " of conduct and Policy on ", noun1, " of Conduct Infringement sections of the ", verb3, " catalog for more ", plural_noun2, ".")
